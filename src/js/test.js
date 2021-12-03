288b259b4ce890776d5af68917db5629cb881e78
Aditya Wibisana <aditya.wibisana@gmail.com>
2021-12-03 16:46:02 +0700

    Add negative test case

--- src/js/test.js
+++ src/js/test.js
@@ -6,14 +6,17 @@ const boxesHorizontalPositiveTest1 = [
     ["X", "X", "X"],
     ["O", "X", "O"],
 ];
+expect(tictactoesolver.isWinningHorizontal(boxesHorizontalPositiveTest1, "X")).toBe(true)
+expect(tictactoesolver.isWinningHorizontal(boxesHorizontalPositiveTest1, "O")).toBe(false)
+
 const boxesHorizontalPositiveTest2 = [
     ["O", "X", "X", "O"],
     ["O", "O", "X", "O"],
     ["O", "X", "X", "X"],
     ["O", "O", "X", "O"],
 ]
-expect(tictactoesolver.isWinningHorizontal(boxesHorizontalPositiveTest1, "X")).toBe(true)
 expect(tictactoesolver.isWinningHorizontal(boxesHorizontalPositiveTest2, "X")).toBe(true)
+expect(tictactoesolver.isWinningHorizontal(boxesHorizontalPositiveTest2, "O")).toBe(false)
 
 console.log("All test has been passed")
 
