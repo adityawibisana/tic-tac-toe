d32950ad6fdab31b10cbd365153c5d9c9d03ae38
Aditya Wibisana <aditya.wibisana@gmail.com>
2021-12-03 17:59:15 +0700

    Rename variable

--- src/js/test.js
+++ src/js/test.js
@@ -1,22 +1,22 @@
 const expect = require('expect');
 const tictactoesolver = require('./tictactoesolver');
 
-const boxesHorizontalPositiveTest1 = [
+const boxesHorizontalTest1 = [
     ["O", "X", "O"],
     ["X", "X", "X"],
     ["O", "X", "O"],
 ];
-expect(tictactoesolver.isWinningHorizontal(boxesHorizontalPositiveTest1, "X")).toBe(true)
-expect(tictactoesolver.isWinningHorizontal(boxesHorizontalPositiveTest1, "O")).toBe(false)
+expect(tictactoesolver.isWinningHorizontal(boxesHorizontalTest1, "X")).toBe(true)
+expect(tictactoesolver.isWinningHorizontal(boxesHorizontalTest1, "O")).toBe(false)
 
-const boxesHorizontalPositiveTest2 = [
+const boxesHorizontalTest2 = [
     ["O", "X", "X", "O"],
     ["O", "O", "X", "O"],
     ["O", "X", "X", "X"],
     ["O", "O", "X", "O"],
 ]
-expect(tictactoesolver.isWinningHorizontal(boxesHorizontalPositiveTest2, "X")).toBe(true)
-expect(tictactoesolver.isWinningHorizontal(boxesHorizontalPositiveTest2, "O")).toBe(false)
+expect(tictactoesolver.isWinningHorizontal(boxesHorizontalTest2, "X")).toBe(true)
+expect(tictactoesolver.isWinningHorizontal(boxesHorizontalTest2, "O")).toBe(false)
 
 console.log("All test has been passed")
 
