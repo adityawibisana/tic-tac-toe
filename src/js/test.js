612114a3494a3d776ddf34e54f9a35ac23ad8ebe
Aditya Wibisana <aditya.wibisana@gmail.com>
2021-12-03 16:36:58 +0700

    Use expect for automated test

--- src/js/test.js
+++ src/js/test.js
@@ -1,10 +1,17 @@
+const expect = require('expect');
 const tictactoesolver = require('./tictactoesolver');
 
 const boxesHorizontalPositiveTest1 = [
     ["O", "X", "O"],
     ["X", "X", "X"],
     ["O", "X", "O"],
+];
+const boxesHorizontalPositiveTest2 = [
+    ["O", "X", "X", "O"],
+    ["O", "O", "O", "O"],
+    ["O", "X", "X", "X"],
+    ["O", "O", "X", "O"],
 ]
-
-console.log(`${tictactoesolver.isWinningHorizontal(boxesHorizontalPositiveTest1, "X")}`)
+expect(tictactoesolver.isWinningHorizontal(boxesHorizontalPositiveTest1, "X")).toBe(true)
+expect(tictactoesolver.isWinningHorizontal(boxesHorizontalPositiveTest2, "X")).toBe(true)
 
