eba55ec698fdbb3894201974a571e2df32918f48
Aditya Wibisana <aditya.wibisana@gmail.com>
2021-12-05 12:37:09 +0700

    Initial setting up socket io

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const express = require('express')
-const path = require('path')
-
 const app = express()
+const http = require('http')
+const server = http.createServer(app)
+const { Server } = require("socket.io")
+const io = new Server(server)
 const port = process.env.PORT || 3000
 
 app.use(express.static(__dirname))
-
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '/index.html'));
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on port:${port}`)
-})
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
+
+server.listen(port, () => {
+    console.log(`Server is listening on:${port}`);
+});
